adee4181ea6b17e591d7811b7bc7e7ffd0176701
Chris Nataros <cnataros@gmail.com>
2017-04-11 21:58:49 -0400

    changed it 3rd

--- Christrash.c
+++ Christrash.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
 //This is a test document by Chris Nataros for githut workshop
 printf("%s\n", );hello world
-see this change?
+changing this change
+adding a new line to show diff.
